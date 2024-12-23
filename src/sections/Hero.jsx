cfc6b43438637c4d13c622f2ed79ececa5490ec4
Saleh Ahmed <saleh@alliancebd.net>
2024-12-23 17:03:09 +0600

    Office day 1 updates

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section className='w-screen h-screen min-h-[600px]' id='hero'>
         <div className="w-full h-[95%] bg-hero-bg bg-cover bg-fixed bg-center flex flex-col justify-center items-center text-center padding-x select-none">
-            <h1 className='text-4xl font-baskerville capitalize text-white leading-normal margin-small'>finest quality animal hide leather</h1>
+            <h1 className='text-4xl md:text-5xl font-baskerville capitalize text-white margin-small leading-normal'>finest quality animal hide leather</h1>
             <h2 className='text-md font-open-sans uppercase text-white'>since 1997</h2>
         </div>
     </section>
